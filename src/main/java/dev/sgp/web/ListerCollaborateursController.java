0aeda0c4ebca60a3b791c8c6f3dc936fb54eeee5
MAWAAW <dark1111@hotmail.fr>
2017-12-13 15:44:57 +0100

    mvc

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -2,6 +2,7 @@ package dev.sgp.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,18 +14,12 @@ public class ListerCollaborateursController extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, 
 			HttpServletResponse resp) throws ServletException, IOException {
+
+		request.setAttribute("listeNoms", Arrays.asList("Julien","Alice","Mehdi"));
 		
-		// recupere la valeur d'un parametre dont le nom est avecPhoto
-		String avecPhotoParam = request.getParameter("avecPhoto");
-		// recupere la valeur d'un parametre dont le nom est departement
-		String departementParam = request.getParameter("departement");
-		resp.setContentType("text/html");
-		// code HTML ecrit dans le corps de la reponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-		+ "<ul>"
-		+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-		+ "<li>departement="+ departementParam + "</li>"
-		+ "</ul>");
+		request.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp")
+			.forward(request,resp);
+		
 	}
 	
 }
